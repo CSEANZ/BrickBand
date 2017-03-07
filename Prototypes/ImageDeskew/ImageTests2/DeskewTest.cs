0bf8285f93adf67c387eb33fc91f0144da6b900d
Jordan Knight <jorkni@microsoft.com>
2017-03-08 10:55:26 +1100

    Found the four corners.

--- Prototypes/ImageDeskew/ImageTests2/DeskewTest.cs
+++ Prototypes/ImageDeskew/ImageTests2/DeskewTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -16,7 +17,7 @@ namespace ImageTests
     [TestClass]
     public class DeskewTest
     {
-       
+
         public async Task TestDeskew()
         {
             //var f = System.Drawing.Image.FromFile("TestSkew.jpg");
@@ -27,7 +28,7 @@ namespace ImageTests
 
             Image<Gray, byte> image = new Image<Gray, byte>(f.FullName);
             double cannyThreshold = 10;
-           double cannyThresholdLinking = 10;
+            double cannyThresholdLinking = 10;
             Gray circleAccumulatorThreshold = new Gray(500);
             Image<Gray, Byte> cannyEdges = image.Canny(cannyThreshold, cannyThresholdLinking);
             LineSegment2D[] lines = cannyEdges.HoughLinesBinary(
@@ -60,11 +61,11 @@ namespace ImageTests
         [TestMethod]
         public async Task Test3()
         {
-            double cannyThreshold = 80;
-            double cannyThresholdLinking = 80;
+            double cannyThreshold = 70;
+            double cannyThresholdLinking = 70;
 
             var file = new FileInfo("TestSkew2.jpg");
-            
+
             var img = System.Drawing.Image.FromFile(file.FullName);
             var bitmap = new Bitmap(img);
             //var smooth = AdaptiveThresholdSmoothFilter(bitmap, 20, 20, 0.1);
@@ -89,7 +90,7 @@ namespace ImageTests
                30, //min Line width
                10); //gap between lines
 
-            
+
 
             Mat lineImage = new Mat(image.Size, DepthType.Cv8U, 3);
             lineImage.SetTo(new MCvScalar(0));
@@ -98,18 +99,162 @@ namespace ImageTests
 
             var points = _findPoints(lines);
 
-            CvInvoke.Line(lineImage, new Point(points.Item1, 0), new Point(points.Item1, image.Size.Height), new Bgr(Color.DodgerBlue).MCvScalar, 2);
-            CvInvoke.Line(lineImage, new Point(points.Item2, 0), new Point(points.Item2, image.Size.Height), new Bgr(Color.DodgerBlue).MCvScalar, 2);
-            CvInvoke.Line(lineImage, new Point(0, points.Item3), new Point(image.Size.Width, points.Item3), new Bgr(Color.DodgerBlue).MCvScalar, 2);
-            CvInvoke.Line(lineImage, new Point(0, points.Item4), new Point(image.Size.Width, points.Item4), new Bgr(Color.DodgerBlue).MCvScalar, 2);
-            
+            foreach (var p in points)
+            {
+                CvInvoke.Circle(lineImage, p.P1, 10, new Bgr(Color.DodgerBlue).MCvScalar, 2);
+            }
+
+
+
+
+
+            //CvInvoke.Line(lineImage, new Point(points.Item1, 0), new Point(points.Item1, image.Size.Height), new Bgr(Color.DodgerBlue).MCvScalar, 2);
+            //CvInvoke.Line(lineImage, new Point(points.Item2, 0), new Point(points.Item2, image.Size.Height), new Bgr(Color.DodgerBlue).MCvScalar, 2);
+            //CvInvoke.Line(lineImage, new Point(0, points.Item3), new Point(image.Size.Width, points.Item3), new Bgr(Color.DodgerBlue).MCvScalar, 2);
+            //CvInvoke.Line(lineImage, new Point(0, points.Item4), new Point(image.Size.Width, points.Item4), new Bgr(Color.DodgerBlue).MCvScalar, 2);
+
 
             var bmp = lineImage.Bitmap;
 
             bmp.Save("output_Lines.jpg", ImageFormat.Jpeg);
+
+
         }
 
-        (int, int, int, int) _findPoints(LineSegment2D[] lines)
+        List<LineSegment2D> _findPoints(LineSegment2D[] lines)
+        {
+
+            var lowXy = int.MaxValue;
+            var lowXyLine = default(LineSegment2D);
+
+           
+            var lowXHighY_X = int.MaxValue;
+            var lowXHighY_Y = 0;
+            var lowXHighY_Line = default(LineSegment2D);
+
+            var highXLowY_X = 0;
+            var highXLowY_Y = int.MaxValue;
+            var highXLowY_Line = default(LineSegment2D);
+
+            var highXHighY_X = 0;
+            var highXHighY_Y = 0;
+            var highXHighY_Line = default(LineSegment2D);
+
+
+            foreach (var line in lines)
+            {
+                
+                //find low x and low y
+                if (line.P1.X + line.P1.Y < lowXy - 5)
+                {
+                    lowXyLine = line;
+                    lowXy = line.P1.X + line.P1.Y;
+                }
+
+                if (line.P2.X + line.P2.Y < lowXy - 5)
+                {
+                    lowXyLine = line;
+                    lowXy = line.P2.X + line.P2.Y;
+                }
+
+                //find low x and High Y
+                if (line.P1.X < lowXHighY_X && line.P1.Y > lowXHighY_Y)
+                {
+                    lowXHighY_X = line.P1.X;
+                    lowXHighY_Y = line.P1.Y;
+                    lowXHighY_Line = line;
+                }
+
+                if (line.P2.X < lowXHighY_X && line.P2.Y > lowXHighY_Y)
+                {
+                    lowXHighY_X = line.P2.X;
+                    lowXHighY_Y = line.P2.Y;
+                    lowXHighY_Line = line;
+                }
+
+                if (line.P1.X > highXLowY_X && line.P1.Y < highXLowY_Y)
+                {
+                    highXLowY_X = line.P1.X;
+                    highXLowY_Y = line.P1.Y;
+                    highXLowY_Line = line;
+                }
+
+                if (line.P2.X > highXLowY_X && line.P2.Y < highXLowY_Y)
+                {
+                    highXLowY_X = line.P2.X;
+                    highXLowY_Y = line.P2.Y;
+                    highXLowY_Line = line;
+                }
+
+                if (line.P1.X > highXHighY_X && line.P1.Y > highXHighY_Y)
+                {
+                    highXHighY_X = line.P1.X;
+                    highXHighY_Y = line.P1.Y;
+                    highXHighY_Line = line;
+                }
+
+                if (line.P2.X > highXHighY_X && line.P2.Y > highXHighY_Y)
+                {
+                    highXHighY_X = line.P2.X;
+                    highXHighY_Y = line.P2.Y;
+                    highXHighY_Line = line;
+                }
+
+
+                //find lines that start or end near the start or end of this line and that have a sufficiently differnt rotation. 
+
+
+
+                //foreach (var lineInner in lines)
+                //{
+                //    if (lineInner.P1 == line.P1 && lineInner.P2 == line.P2)
+                //    {
+                //        continue;
+                //    }
+
+                //    if (_isNear(line, lineInner))
+                //    {
+                //        corners.Add(line);
+                //    }
+
+                //}
+
+                //var lowerX = _findLower(line.P1.X, line.P2.X);
+
+                //if (lowerX < leftX)
+                //{
+                //    leftX = lowerX;
+                //}
+
+                //var upperX = _findUpper(line.P1.X, line.P2.X);
+
+                //if (upperX > rightX)
+                //{
+                //    rightX = upperX;
+                //}
+
+                //var upperY = _findUpper(line.P1.Y, line.P2.Y);
+                ////upper as in up the image, so lower value :/
+                //if (upperY < topY)
+                //{
+                //    topY = upperY;
+                //}
+
+                //var lowerY = _findLower(line.P1.Y, line.P2.Y);
+
+                //if (lowerY > bottomY)
+                //{
+                //    bottomY = lowerY;
+                //}
+            }
+
+            return new List<LineSegment2D> { lowXyLine, lowXHighY_Line, highXLowY_Line, highXHighY_Line };
+
+            //return corners;
+
+        }
+
+        List<LineSegment2D> _findCorners(LineSegment2D[] lines)
         {
             int leftX = int.MaxValue;
             int rightX = 0;
@@ -125,39 +270,103 @@ namespace ImageTests
             //    leftPoint = lowerX;
             //}
 
+            var corners = new List<LineSegment2D>();
+
 
             foreach (var line in lines)
             {
-                var lowerX = _findLower(line.P1.X, line.P2.X);
+                //find lines that start or end near the start or end of this line and that have a sufficiently differnt rotation. 
 
-                if (lowerX < leftX)
+                foreach (var lineInner in lines)
                 {
-                    leftX = lowerX;
+                    if (lineInner.P1 == line.P1 && lineInner.P2 == line.P2)
+                    {
+                        continue;
+                    }
+
+                    if (_isNear(line, lineInner))
+                    {
+                        corners.Add(line);
+                    }
+
                 }
 
-                var upperX = _findUpper(line.P1.X, line.P2.X);
+                //var lowerX = _findLower(line.P1.X, line.P2.X);
 
-                if (upperX > rightX)
-                {
-                    rightX = upperX;
-                }
+                //if (lowerX < leftX)
+                //{
+                //    leftX = lowerX;
+                //}
 
-                var upperY = _findUpper(line.P1.Y, line.P2.Y);
-                //upper as in up the image, so lower value :/
-                if (upperY < topY)
-                {
-                    topY = upperY;
-                }
+                //var upperX = _findUpper(line.P1.X, line.P2.X);
 
-                var lowerY = _findLower(line.P1.Y, line.P2.Y);
+                //if (upperX > rightX)
+                //{
+                //    rightX = upperX;
+                //}
 
-                if (lowerY > bottomY)
-                {
-                    bottomY = lowerY;
-                }
+                //var upperY = _findUpper(line.P1.Y, line.P2.Y);
+                ////upper as in up the image, so lower value :/
+                //if (upperY < topY)
+                //{
+                //    topY = upperY;
+                //}
+
+                //var lowerY = _findLower(line.P1.Y, line.P2.Y);
+
+                //if (lowerY > bottomY)
+                //{
+                //    bottomY = lowerY;
+                //}
             }
-            return (leftX, rightX, topY, bottomY);
+            return corners;
 
+        }
+
+        bool _isNear(LineSegment2D line, LineSegment2D line2)
+        {
+            Debug.WriteLine($"Direction X: {line.Direction.X}, Direction Y: {line.Direction.Y}");
+
+            var isNear = _isNearPoint(line.P1, line2.P1) || _isNearPoint(line.P2, line2.P2) || _isNearPoint(line.P1, line2.P2);
+
+            if (!isNear)
+            {
+                return false;
+            }
+
+            return !_isNearRotation(line.Direction, line2.Direction);
+        }
+
+        private int nearness = 5;
+        private int rotationNess = 10;
+
+        bool _isNearRotation(PointF a, PointF b)
+        {
+            var dangleInRadians = Math.Atan2(1, 1) - Math.Atan2(a.Y, a.X);
+            var dangleInRadians2 = Math.Atan2(1, 1) - Math.Atan2(b.Y, b.X);
+
+            var isNearRotation = (dangleInRadians > dangleInRadians2 && dangleInRadians - dangleInRadians2 < rotationNess) ||
+                 (dangleInRadians2 > dangleInRadians && dangleInRadians2 - dangleInRadians < rotationNess);
+
+            return isNearRotation;
+        }
+
+        bool _isNearPoint(Point p1, Point p2)
+        {
+            return _isNearPoint(p1.X, p2.X) ||
+                   _isNearPoint(p1.X, p2.Y) ||
+                   _isNearPoint(p1.Y, p2.X) ||
+                   _isNearPoint(p1.Y, p2.Y);
+        }
+
+        bool _isNearPoint(int p1, int p2)
+        {
+            if ((p1 > p2 && p1 - p2 < nearness) || (p2 > p1 && p2 - p1 < nearness))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         int _findUpper(int a, int b)
@@ -182,7 +391,7 @@ namespace ImageTests
         public static Image<Gray, byte> Deskew(string file)
         {
             Image<Gray, byte> image = new Image<Gray, byte>(file);
-            
+
             var bw = AdaptiveThresholdSmoothFilter(image.ToBitmap(), 20, 20, 0.03); //
             //CvInvoke.cvShowImage("", bw.Clone());
             LineSegment2D[] lines = bw.HoughLinesBinary(
@@ -215,7 +424,7 @@ namespace ImageTests
             var smoothedGray = gray.SmoothBlur(width, height);
 
             var bw = smoothedGray - gray - c;
-            
+
             return bw.ThresholdBinary(new Gray(0), new Gray(255));
         }
     }
